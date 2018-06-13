3c27212baee06f8950e32a980e0e2925491bb272
Hugo Pakula <hugo@hugopakula.com>
2018-06-12 23:44:32 -0500

    Add routes

--- app.js
+++ app.js
@@ -1,41 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+app.set("view engine", "pug");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', function(req, res, next){
+    //build all things above send. you can only use send once
+    console.log("YO wabba wabba");
+    console.warn("This is kinda bad");
+    console.error("What does this do?");
+    res.render("index", {pageTitle: "Home"});
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/restoration', function(req, res, next) {
+    res.render("restoration", {pageTitle: "Restoration"});
 });
 
-module.exports = app;
+app.get('/about', function(req, res, next) {
+
+});
+
+app.get('/service', function(req, res, next) {
+
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(3180, function(){
+    console.log("Server is Running on Port 3180");
+});
